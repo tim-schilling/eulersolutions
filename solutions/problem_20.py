4e2bafe2aaca4b19f7138cdb48a1ae13b5dda528
tschilling <schillingt@better-simple.com>
2013-10-16 19:44:08 -0400

    Fixing some pep 8 issues.

--- solutions/problem_20.py
+++ solutions/problem_20.py
@@ -1,15 +1,16 @@
 def factorial(x):
-     if x == 0:
-         return 1
-     return x * factorial(x-1)
+    if x == 0:
+        return 1
+    return x * factorial(x-1)
+
 
 def sum_digits(value):
-    sum = 0
+    summed_value = 0
     while value > 10:
-        sum += value % 10
+        summed_value += value % 10
         value = long(value / 10)
-    sum += value
-    return sum
+    summed_value += value
+    return summed_value
 
 digits = factorial(100)
 print sum_digits(digits)
